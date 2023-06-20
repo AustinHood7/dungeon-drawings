a0c56af805969b6b97fe61278042ab99791d85eb
austin.hood <Austinmhood@live.com>
2023-06-20 04:39:27 -0400

    Added color palette and styled half of landing page including a custom gradient

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,10 @@ export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      backgroundImage: theme => ({
+        'gradient-radial': 'radial-gradient(ellipse at center, red, yellow, green)', // radial gradient
+        'gradient-linear': 'linear-gradient(to bottom right, #121212, hsla(168, 96%, 50%, 0.3) 99%)', // linear gradient
+     }),
       colors: {
         'bg': '#121212',
         'primary': '#959797',
